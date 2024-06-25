68ef30cf1a43780bd7825ff8f57e8fa7d9812d21
Mohankumar <mkmohankumar261998@gmail.com>
2024-06-25 22:39:59 +0530

    Test commit

--- qtify/src/components/BasicComponents/Divider.jsx
+++
@@ -1,4 +0,0 @@
-import styles from "./basicComponents.module.css";
-export const Divider = () => {
-  return <hr className={styles.horizontalDivider} />;
-};
--- qtify/src/components/Navbar/NavBar.jsx
+++ qtify/src/components/Navbar/NavBar.jsx
@@ -1,19 +1,22 @@
-import Button from "../BasicComponents/Button";
-import { SearchBar } from "../BasicComponents/SearchBar";
-import style from "./navbar.module.css";
+import React from "react";
+import styles from "./Navbar.module.css";
 import Logo from "../Logo/Logo";
-import { Link } from "react-router-dom";
+import SearchBar from "../SearchBar/SearchBar";
+import Button from "../Button/Button";
 
 const Navbar = () => {
-  return (
-    <nav className={style.navbarContainer}>
-      <Link to="/">
-        <Logo />
-      </Link>
-      <SearchBar />
-      <Button appearance="secondary">Give Feedback</Button>
-    </nav>
-  );
-};
+
+    return (
+        <>
+            <nav className={styles.navbar}>
+                <Logo />
+                <SearchBar search={"Search a song of your choice"} />
+                <Button children={"Give Feedback"} />
+            </nav>
+
+        </>
+
+    );
+}
 
 export default Navbar;
